0912db370c79dc9ad41086f57a08f2d6311efe80
Phantom-VK <vikramaditya.khupse@bettrhq.com>
2026-04-02 22:39:26 +0530

    refactor: use FastAPI lifespan startup

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.config import APP_TITLE, APP_VERSION
@@ -6,12 +8,12 @@ from app import models
 from app.routers.health import router as health_router
 from app.routers.utils import router as utils_router
 
-app = FastAPI(title=APP_TITLE, version=APP_VERSION)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(title=APP_TITLE, version=APP_VERSION, lifespan=lifespan)
 app.include_router(health_router)
 app.include_router(utils_router)
-
-
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
-
